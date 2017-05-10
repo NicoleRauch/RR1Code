3eed55bbc44f3856d3f6e9ab57b636851e80381f
Nicole Rauch <nicole.m@gmx.de>
2017-05-10 23:48:36 +0200

    Wir brauchen den auskommentierten Code am Anfang nicht mehr.

--- src/index.js
+++ src/index.js
@@ -1,27 +1,6 @@
 import React, {Component} from "react";
 import ReactDOM from "react-dom";
 
-/*
-import {Provider} from "react-redux";
-import {createStore, applyMiddleware} from "redux";
-import thunkMiddleware from 'redux-thunk';
-import logger from "redux-logger";
-
-import App from "./App";
-import reducers from './reducers';
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(reducers,
- composeEnhancers(
-  applyMiddleware(
-    thunkMiddleware,
-    logger
-  )
- )
-);
-*/
-
 ReactDOM.render(
     <p>Hello React.js!</p>
   , document.getElementById("start")
