afb94b64e15d66abe62e12ac6bb3d7030cf1ac44
Nicole Rauch <nicole.m@gmx.de>
2017-05-10 23:48:06 +0200

    Alte Lösungsfiles, die ersetzt wurden.

--- src-solution/UserList.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import UserListEntry from "./UserListEntry";
-
-export default ({users, handleNameClick, highlightedUser}) => (
-  <ul>
-    { users.map((user, index) => <UserListEntry user={user} highlighted={highlightedUser === index} onNameClick={() => handleNameClick(index)} key={"userlist_"+index} />) }
-  </ul>
-);
