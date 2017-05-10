255f52343b9e38323a3b8f279bf289948c5ce3f3
Nicole Rauch <nicole.m@gmx.de>
2017-05-10 23:47:24 +0200

    Die asynchronen Actions isoliert.

--- src-solution/Actions.js
+++ src-solution/Actions.js
@@ -1,39 +1,8 @@
 import * as backend from "./backend";
 
-export const USER_SELECTED = "USER_SELECTED";
-export const FIRST_NAME_SAVED = "FIRST_NAME_SAVED";
-export const LAST_NAME_SAVED = "LAST_NAME_SAVED";
-export const EMAIL_SAVED = "EMAIL_SAVED";
 export const USER_ADDED = "USER_ADDED";
 export const USERS_SET = "USERS_SET";
 
-export function selectUser(index) {
-  return {
-      type: USER_SELECTED,
-      payload: index
-  };
-}
-
-export function saveFirstName(name) {
-  return {
-    type: FIRST_NAME_SAVED,
-    payload: name
-  }
-}
-
-export function saveLastName(name) {
-  return {
-    type: LAST_NAME_SAVED,
-    payload: name
-  }
-}
-
-export function saveEMail(address) {
-  return {
-    type: EMAIL_SAVED,
-    payload: address
-  }
-}
 
 export function addUser(user) {
   return {
@@ -45,11 +14,11 @@ export function addUser(user) {
 export function submitUser(user) {
   return (dispatch) => {
 
-    //backend.postUser(user, returnCode => {
-      //if(returnCode === 200) {
+    backend.postUser(user, returnCode => {
+      if(returnCode === 200) {
         dispatch(addUser(user));
-      //}
-    //});
+      }
+    });
   };
 }
 
@@ -62,10 +31,8 @@ export function setUsers(users) {
 
 export function loadUsers() {
   return (dispatch) => {
-    /*
     backend.fetchUsers(users => {
         dispatch(setUsers(users));
     });
-    */
   };
 }
